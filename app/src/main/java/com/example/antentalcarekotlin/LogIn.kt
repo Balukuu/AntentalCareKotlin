8fccf777b90fddc6161055d22c34ada6f3e65ce5
Balukuu <edgarbaluku@gmail.com>
2021-09-09 11:16:34 +0300

    first commit

---
+++ app/src/main/java/com/example/antentalcarekotlin/LogIn.kt
@@ -0,0 +1,11 @@
+package com.example.antentalcarekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
